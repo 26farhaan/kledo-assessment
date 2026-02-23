e3711323d1a88f5f358dd4d609c4b552fa45b453
26farhaan <farhan.21144@gmail.com>
2026-02-23 16:11:08 +0700

    fix error build routes file

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,3 @@
 import { type RouteConfig, index, route } from '@react-router/dev/routes';
 
-export default [
-  index('routes/filter.tsx'),
-  route('filter', 'routes/filter.tsx'),
-] satisfies RouteConfig;
+export default [index('routes/filter.tsx')] satisfies RouteConfig;
